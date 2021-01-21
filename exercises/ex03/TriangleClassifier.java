b1cae24ca1951855afd95fba060e25e1110427c4
Alberto Martín López <alberto.martin95@gmail.com>
2021-01-21 17:26:50 +0100

    Minor change to TriangleClassifier code

--- exercises/ex03/TriangleClassifier.java
+++ exercises/ex03/TriangleClassifier.java
@@ -5,6 +5,10 @@ import ex03.TriangleType;
 public class TriangleClassifier {
     public static String classify(int a, int b, int c) {
 
+        System.out.println("This program takes as input three integers and returns " +
+                "the type of triangle that can be built with them.");
+        System.out.println("-------");
+
         if (a <= 0 || b <= 0 || c <= 0) {
             return "NOT_A_TRIANGLE";
         }
@@ -28,28 +32,28 @@ public class TriangleClassifier {
         }
     }
 
-    public static TriangleType classify_v2(int a, int b, int c) {
-
-        if (a <= 0 || b <= 0 || c <= 0) {
-            return TriangleType.NOT_A_TRIANGLE;
-        }
-
-        if (a == b && b == c) {
-            return TriangleType.EQUILATERAL;
-        }
-
-        int max = Math.max(a, Math.max(b, c));
-
-        if ((max == a && max - b - c >= 0) ||
-                (max == b && max - a - c >= 0) ||
-                (max == c && max - a - b >= 0)) {
-            return  TriangleType.NOT_A_TRIANGLE;
-        }
-
-        if (a == b || b == c || a == c) {
-            return TriangleType.ISOSCELES;
-        } else {
-            return TriangleType.SCALENE;
-        }
-    }
+//    public static TriangleType classify_v2(int a, int b, int c) {
+//
+//        if (a <= 0 || b <= 0 || c <= 0) {
+//            return TriangleType.NOT_A_TRIANGLE;
+//        }
+//
+//        if (a == b && b == c) {
+//            return TriangleType.EQUILATERAL;
+//        }
+//
+//        int max = Math.max(a, Math.max(b, c));
+//
+//        if ((max == a && max - b - c >= 0) ||
+//                (max == b && max - a - c >= 0) ||
+//                (max == c && max - a - b >= 0)) {
+//            return  TriangleType.NOT_A_TRIANGLE;
+//        }
+//
+//        if (a == b || b == c || a == c) {
+//            return TriangleType.ISOSCELES;
+//        } else {
+//            return TriangleType.SCALENE;
+//        }
+//    }
 }
